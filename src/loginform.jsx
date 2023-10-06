e69dbed3cc7b6d9ef8ff788245b5ee9d2b8e0608
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-10-06 01:07:27 -0700

    Add files via upload

---
+++ src/loginform.jsx
@@ -0,0 +1,33 @@
+import "App.js";
+
+import "index.js";
+import { useState } from "react";
+
+function Login_Form(){
+const  [loginform, setloginform] = useState("")
+const [checkEmail, setcheckEmail ] = useState("")
+
+const Email = (e) => {
+e.Email
+
+
+
+
+}
+ 
+return{
+
+
+
+}
+
+}
+
+
+
+
+export default Login_Form;
+
+
+
+
