9c0c47cea8e33a90812a803c4a8ec4a7de1da159
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-02-28 13:30:14 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -98,47 +98,39 @@ return (
 export default App;
 ;
 
+copy this into the new about page
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function App() {
-  //return (
-    //<div className="App">
-      //<header className="App-header">
-        //<img src={logo} className="App-logo" alt="logo" />
-        //<p>
-         // Edit <code>src/App.js</code> and save to reload.
-        //</p>
-        //<a
-          //className="App-link"
-          //href="https://reactjs.org"
-          //target="_blank"
-          //rel="noopener noreferrer"
-        //>
-          //Learn React
-        
-       // </a>
-      //<a>
-        
-      //</a>
-      //</header>
-    //</div>
+<TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
   
-  
-  //);
-//}
 
-//export default App;
+</div>
+
+<div className='mt-8'>
+  {skills.find((t) => t.id ===tab).content}
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
